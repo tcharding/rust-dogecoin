2cda9d717536bc2d2369fd1975766501fc9f4466
Tobin C. Harding <me@tobin.cc>
2024-08-13 06:56:43 +1000

    Introduce ScriptBuf extension traits
    In preparation for moving the `ScriptBuf` type to the `primitives`
    crate; introduce two `ScriptBuf` extension traits, one public and one
    private.
    
    Note, the private extension trait has `pub(crate)` where as before this
    patch was applied we use `pub(in crate::script)`. This is because the
    macro doesn't handle the latter syntax.
    
    Note also, that the build failure when `ScriptBuf::from_hex` is not
    found may be confusing because of the history of `from_hex` - users may
    go looking for a `FromHex` trait. We should keep this in mind when
    documenting how to use all the new extension traits.

--- bitcoin/src/blockdata/script/owned.rs
+++ bitcoin/src/blockdata/script/owned.rs
@@ -110,28 +110,28 @@ impl ScriptBuf {
     pub fn extend_from_slice(&mut self, other: &[u8]) { self.0.extend_from_slice(other) }
 }
 
-mod tmp_pub {
-    use super::*;
-    impl ScriptBuf {
+crate::internal_macros::define_extension_trait! {
+    /// Extension functionality for the [`ScriptBuf`] type.
+    pub trait ScriptBufExt impl for ScriptBuf {
         /// Creates a new script builder
-        pub fn builder() -> Builder { Builder::new() }
+        fn builder() -> Builder { Builder::new() }
 
         /// Generates OP_RETURN-type of scriptPubkey for the given data.
-        pub fn new_op_return(data: impl AsRef<PushBytes>) -> Self {
+        fn new_op_return(data: impl AsRef<PushBytes>) -> Self {
             Builder::new().push_opcode(OP_RETURN).push_slice(data).into_script()
         }
 
         /// Creates a [`ScriptBuf`] from a hex string.
-        pub fn from_hex(s: &str) -> Result<Self, hex::HexToBytesError> {
+        fn from_hex(s: &str) -> Result<ScriptBuf, hex::HexToBytesError> {
             let v = Vec::from_hex(s)?;
             Ok(ScriptBuf::from_bytes(v))
         }
 
         /// Adds a single opcode to the script.
-        pub fn push_opcode(&mut self, data: Opcode) { self.push(data.to_u8()); }
+        fn push_opcode(&mut self, data: Opcode) { self.push(data.to_u8()); }
 
         /// Adds instructions to push some arbitrary data onto the stack.
-        pub fn push_slice(&mut self, data: impl AsRef<PushBytes>) {
+        fn push_slice(&mut self, data: impl AsRef<PushBytes>) {
             let data = data.as_ref();
             self.reserve(Self::reserved_len_for_slice(data.len()));
             self.push_slice_no_opt(data);
@@ -143,7 +143,7 @@ mod tmp_pub {
         ///
         /// The method panics if the instruction is a data push with length greater or equal to
         /// 0x100000000.
-        pub fn push_instruction(&mut self, instruction: Instruction<'_>) {
+        fn push_instruction(&mut self, instruction: Instruction<'_>) {
             match instruction {
                 Instruction::Op(opcode) => self.push_opcode(opcode),
                 Instruction::PushBytes(bytes) => self.push_slice(bytes),
@@ -151,7 +151,7 @@ mod tmp_pub {
         }
 
         /// Like push_instruction, but avoids calling `reserve` to not re-check the length.
-        pub fn push_instruction_no_opt(&mut self, instruction: Instruction<'_>) {
+        fn push_instruction_no_opt(&mut self, instruction: Instruction<'_>) {
             match instruction {
                 Instruction::Op(opcode) => self.push_opcode(opcode),
                 Instruction::PushBytes(bytes) => self.push_slice_no_opt(bytes),
@@ -172,15 +172,14 @@ mod tmp_pub {
         /// This function needs to iterate over the script to find the last instruction. Prefer
         /// `Builder` if you're creating the script from scratch or if you want to push `OP_VERIFY`
         /// multiple times.
-        pub fn scan_and_push_verify(&mut self) { self.push_verify(self.last_opcode()); }
+        fn scan_and_push_verify(&mut self) { self.push_verify(self.last_opcode()); }
     }
 }
 
-mod tmp_priv {
-    use super::*;
-    impl ScriptBuf {
+crate::internal_macros::define_extension_trait! {
+    pub(crate) trait ScriptBufExtPriv impl for ScriptBuf {
         /// Pushes the slice without reserving
-        pub(in crate::blockdata::script) fn push_slice_no_opt(&mut self, data: &PushBytes) {
+        fn push_slice_no_opt(&mut self, data: &PushBytes) {
             // Start with a PUSH opcode
             match data.len().to_u64() {
                 n if n < opcodes::Ordinary::OP_PUSHDATA1 as u64 => {
@@ -209,7 +208,7 @@ mod tmp_priv {
         }
 
         /// Computes the sum of `len` and the length of an appropriate push opcode.
-        pub(in crate::blockdata::script) fn reserved_len_for_slice(len: usize) -> usize {
+        fn reserved_len_for_slice(len: usize) -> usize {
             len + match len {
                 0..=0x4b => 1,
                 0x4c..=0xff => 2,
@@ -223,7 +222,7 @@ mod tmp_priv {
         /// alternative.
         ///
         /// See the public fn [`Self::scan_and_push_verify`] to learn more.
-        pub(in crate::blockdata::script) fn push_verify(&mut self, last_opcode: Option<Opcode>) {
+        fn push_verify(&mut self, last_opcode: Option<Opcode>) {
             match opcode_to_verify(last_opcode) {
                 Some(opcode) => {
                     self.pop();
